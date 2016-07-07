cf6c3a2d7f21599142a3dc46eaab198b7bdc983c
Tom Ballingall <tom.ballingall@gmail.com>
2016-07-06 22:15:25 -0400

    Added devise users and routes. Specs to come.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :recipes
+  has_many :ingredients
+  has_many :kegs
+  
 end
